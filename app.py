7c19d6cdf8e0f6237792417ae7bfb6743274bb04
Jan Binkowski <jan.binkowski@wp.pl>
2021-06-03 21:20:11 +0200

    Implement hash password function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, json, session
 from flask_mysqldb import MySQL
 from werkzeug.utils import redirect
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app.config['MYSQL_HOST'] = '34.116.254.11'
 app.config['MYSQL_USER'] = 'jan-binkowski'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sklep'
-# app.config['MYSQL_PORT'] = '3306'
+
 mysql = MySQL(app)
 app.secret_key = 'why would I tell you my secret key?'
 
@@ -46,15 +46,18 @@ def validateLogin():
         cursor = mysql.connection.cursor()
         # cursor.callproc('sprawdz_login', (login,haslo,))
         cursor.execute(
-            '''	select * from Dane_uzytkownika where login = '%s' and haslo = '%s';''' % (
-            login, haslo))
+            '''	select * from Dane_uzytkownika where login = '%s';''' % (
+            login, ))
 
         data = cursor.fetchall()
 
         if len(data) > 0:
-            # session['user'] = data[0][0]
-            # return redirect('/userHome', userName='mareczekjakis')
-            return render_template('userHome.html', userName=login)
+            if check_password_hash(str(data[0][4]), haslo):
+                session['user'] = data[0][0]
+
+                return render_template('/userHome.html', userName = login)
+            else:
+                return render_template('error.html', error='Wrong Login or Password.')
         else:
             return render_template('error.html', error='Wrong Login or Password.')
 
@@ -79,13 +82,14 @@ def signUp():
         numer_domu = request.form['numer_domu']
         numer_mieszkania = request.form['numer_mieszkania']
 
+        hashed_haslo = generate_password_hash(haslo)
 
         cursor = mysql.connection.cursor()
 
         args = [imie, nazwisko, login, haslo, panstwo, miasto, wojewodztwo, ulica, kod_pocztowy, numer_domu]
         # cursor.callproc('dodaj_uzytkownika', (imie, nazwisko, login, haslo, panstwo, miasto, wojewodztwo, ulica, kod_pocztowy, numer_domu))
 
-        cursor.execute('''INSERT INTO Dane_uzytkownika (imie, nazwisko, login, haslo, panstwo, miasto, wojewodztwo, ulica, kod_pocztowy, numer_domu) VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');'''%(imie, nazwisko, login, haslo, panstwo, miasto, wojewodztwo, ulica, kod_pocztowy, numer_domu))
+        cursor.execute('''INSERT INTO Dane_uzytkownika (imie, nazwisko, login, haslo, panstwo, miasto, wojewodztwo, ulica, kod_pocztowy, numer_domu) VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');'''%(imie, nazwisko, login, hashed_haslo, panstwo, miasto, wojewodztwo, ulica, kod_pocztowy, numer_domu))
         # cursor.execute('''CALL dodaj_uzytkownika('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');'''%(imie, nazwisko, login, haslo, panstwo, miasto, wojewodztwo, ulica, kod_pocztowy, numer_domu))
 
         mysql.connection.commit()
